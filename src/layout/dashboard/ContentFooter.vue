785f8054df9e112951f71fed320d7661c842ea0b
Luis Duarte <cunha.felipe96@gmail.com>
2019-11-12 21:44:53 -0300

    firts commit

---
+++ src/layout/dashboard/ContentFooter.vue
@@ -0,0 +1,24 @@
+<template>
+  <footer class="footer">
+    <div class="container-fluid">
+      <ul class="nav">
+        <li class="nav-item">
+          <a class="nav-link" href="/">
+            Stock Control
+          </a>
+        </li>        
+      </ul>      
+    </div>
+  </footer>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        year: new Date().getFullYear()
+      }
+    }
+  };
+</script>
+<style>
+</style>
